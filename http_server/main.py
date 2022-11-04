0322fb6fbf08f44afe1a123af83215c7c11a18e0
Guanchao Samuel Huang <samuel.g.huang@outlook.com>
2022-11-04 12:36:06 +0800

    Change student ID

--- http_server/main.py
+++ http_server/main.py
@@ -77,8 +77,7 @@ def task5_session_getimage(server: HTTPServer, request: HTTPRequest, response: H
     pass
 
 
-# TODO: Change this to your student ID, otherwise you may lost all of your points
-YOUR_STUDENT_ID = 12010000
+YOUR_STUDENT_ID = 11912309
 
 http_server = HTTPServer(config.LISTEN_PORT)
 http_server.register_handler("/", default_handler)
